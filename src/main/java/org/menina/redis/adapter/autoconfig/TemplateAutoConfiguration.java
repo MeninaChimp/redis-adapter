cf8ee47b0b87a4433df3c6dca5612e59cb348b27
MeninaChimp <zh179092007@gmail.com>
2017-07-19 00:22:37 +0800

    负载均衡方式，序列化方式可配置

--- src/main/java/org/menina/redis/adapter/autoconfig/TemplateAutoConfiguration.java
+++ src/main/java/org/menina/redis/adapter/autoconfig/TemplateAutoConfiguration.java
@@ -1,12 +1,12 @@
 package org.menina.redis.adapter.autoconfig;
 
 import lombok.extern.slf4j.Slf4j;
-import org.menina.redis.adapter.autoconfig.conditions.CodisConditon;
-import org.menina.redis.adapter.autoconfig.conditions.ConditionOnMissingRedisTemplate;
-import org.menina.redis.adapter.autoconfig.conditions.ReadWriteCondition;
 import org.menina.redis.adapter.balance.Balance;
+import org.menina.redis.adapter.extend.ExtendOperationSupport;
+import org.menina.redis.adapter.extend.RedisTemplateDelegator;
 import org.menina.redis.adapter.pool.CodisPool;
 import org.menina.redis.adapter.pool.PoolFactory;
+import org.menina.redis.adapter.properties.CommonProperties;
 import org.menina.redis.adapter.serialize.Serializer;
 import org.menina.redis.adapter.template.RedisTemplate;
 import org.menina.redis.adapter.utils.WriteOperation;
@@ -15,7 +15,6 @@ import org.springframework.cglib.proxy.Enhancer;
 import org.springframework.cglib.proxy.MethodInterceptor;
 import org.springframework.cglib.proxy.MethodProxy;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 
 import java.lang.reflect.Method;
@@ -23,7 +22,6 @@ import java.lang.reflect.Method;
 /**
  * author: Menina
  */
-
 @Configuration
 @Slf4j
 public class TemplateAutoConfiguration {
@@ -34,66 +32,61 @@ public class TemplateAutoConfiguration {
     @Autowired
     private PoolFactory defaultPoolFactory;
 
-    @Autowired (required = false)
+    @Autowired
+    private CommonProperties commonProperties;
+
+    @Autowired(required = false)
     private Serializer serializer;
 
     @Autowired (required = false)
     private Balance balance;
 
     @Bean
-    @Conditional(CodisConditon.class)
     public RedisTemplate redisTemplateForCodis() {
-        RedisTemplate template = new RedisTemplate();
-        CodisPool codisPool = (CodisPool)defaultPoolFactory.getCodisPool();
-        if(null != this.balance){
-            codisPool.setBalanceStrategy(balance);
-        }
-
-        if(null != this.serializer){
-            template.setSerializer(serializer);
-        }
-
-        template.setDataSource(codisPool);
-        return template;
-    }
-
-    @Bean
-    @Conditional(ReadWriteCondition.class)
-    public RedisTemplate redisTemplateForReadWrite() {
-        this.readRedisTemplate = new RedisTemplate();
-        this.writeRedisTemplate = new RedisTemplate();
-        this.readRedisTemplate.setDataSource(defaultPoolFactory.getReadPool());
-        this.writeRedisTemplate.setDataSource(defaultPoolFactory.getWritePool());
-        if(null != this.serializer){
-            this.readRedisTemplate.setSerializer(serializer);
-            this.writeRedisTemplate.setSerializer(serializer);
-        }
-
-        Enhancer enhancer = new Enhancer();
-        enhancer.setSuperclass(RedisTemplate.class);
-        enhancer.setCallback(new MethodInterceptor() {
-            @Override
-            public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
-                if (WriteOperation.isWriteOperation(method.getName())) {
-                    return method.invoke(writeRedisTemplate, objects);
-                }
-
-                return method.invoke(readRedisTemplate, objects);
+        if(this.commonProperties.isEnableCodisConfig()){
+            RedisTemplate template = new RedisTemplate();
+            CodisPool codisPool = (CodisPool)defaultPoolFactory.getCodisPool();
+            if(null != this.balance){
+                codisPool.setBalanceStrategy(balance);
             }
-        });
 
-        return (RedisTemplate) enhancer.create();
-    }
-
-    @Bean
-    @Conditional(ConditionOnMissingRedisTemplate.class)
-    public RedisTemplate redisTemplate() {
-        RedisTemplate template = new RedisTemplate();
-        if(null != this.serializer){
-            template.setSerializer(serializer);
+            template.setDataSource(codisPool);
+            return template;
         }
 
+        if(this.commonProperties.isEnableReadwriteConfig()){
+            this.readRedisTemplate = new RedisTemplate();
+            this.writeRedisTemplate = new RedisTemplate();
+            this.readRedisTemplate.setDataSource(defaultPoolFactory.getReadPool());
+            this.writeRedisTemplate.setDataSource(defaultPoolFactory.getWritePool());
+            Enhancer enhancer = new Enhancer();
+            enhancer.setSuperclass(RedisTemplate.class);
+            enhancer.setCallback(new MethodInterceptor() {
+                @Override
+                public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
+                    if (WriteOperation.isWriteOperation(method.getName())) {
+                        return method.invoke(writeRedisTemplate, objects);
+                    }
+
+                    return method.invoke(readRedisTemplate, objects);
+                }
+            });
+
+            return (RedisTemplate) enhancer.create();
+        }
+
+        RedisTemplate template = new RedisTemplate();
         template.setDataSource(defaultPoolFactory.getPool());
         return template;
+    }
+
+    @Bean
+    public ExtendOperationSupport redisTemplateDelegator(){
+        RedisTemplateDelegator delegator = new RedisTemplateDelegator();
+        if(null != this.serializer){
+            delegator.setSerializer(serializer);
+        }
+
+        return delegator;
     }
 }
